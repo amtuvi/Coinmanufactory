ae2059885a92b60ac055655f611e1d3bb0e642c0
davidjunker <88778017+davidjunker@users.noreply.github.com>
2021-09-06 12:03:06 +0200

    Add files via upload

---
+++ src/scripts/contactform.js
@@ -0,0 +1,12 @@
+var contactForm = document.querySelector('.contact-form');
+var contactFormContainer = document.querySelector('.contact-form-container');
+var successText = document.querySelector('.contact-success-text');
+
+contactForm.addEventListener('submit', function(){
+    contactFormContainer.classList.add('contact-fly-out');
+
+    setTimeout(function () {
+        contactFormContainer.style.display = "none";
+        successText.style.display = "block";
+    }, 500);
+})
