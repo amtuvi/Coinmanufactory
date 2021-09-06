0e07ff29885b573df6e807f538a73a407035364d
davidjunker <88778017+davidjunker@users.noreply.github.com>
2021-09-06 11:39:59 +0200

    Delete contactform.js

--- src/scripts/contactform.js
+++
@@ -1,8 +0,0 @@
-var contactForm = document.querySelector('.token-form');
-var contactFormContainer = document.querySelector('.contact-form-container');
-var successText = document.querySelector('.contact-success-text');
-
-contactForm.addEventListener('submit', function(){
-    contactFormContainer.style.display = "none";
-    successText.style.display = "block";
-})
